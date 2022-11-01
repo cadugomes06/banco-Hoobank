6e641b6d54d85f52ed64c24cb01bf3054369c00b
Carlos Eduardo <cadu.010808@gmail.com>
2022-10-31 21:41:18 -0300

    Navbar for desktop and mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './style'
+import { Navbar, Hero, Business, Billing, Stats, CardDeal,
+   Testimonials, Clients, CTA, Footer } from './components'
 
 
 const App = () => {
@@ -8,9 +10,29 @@ return (
     <div className='bg-primary w-full overflow-hidden'>
        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
          <div className={`${styles.boxWidth}`}>
-            Navbar
+            <Navbar />
          </div>
        </div>
+
+       <div className={`bg-primary ${styles.flexStart}`}>
+         <div className={`${styles.boxWidth}`}>
+              <Hero />
+         </div>
+       </div>
+
+       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+         <div className={`${styles.boxWidth}`}>
+            <Business />
+            <Billing />
+            <Stats />
+            <CardDeal />
+            <Testimonials />
+            <Clients />
+            <CTA />
+            <Footer />                            
+         </div>
+       </div>
+
     </div>
   )
 }
